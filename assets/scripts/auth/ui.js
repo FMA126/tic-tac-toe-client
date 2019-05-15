98d3792f8c108f644b429e6b095be669cce349b0
FMA126 <mhblanks@gmail.com>
2019-05-15 14:31:44 -0400

    Issue with merging master
    Attempting to fix merge issue

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,51 @@
+const store = require('../store')
+
+const onSignUpSuccess = responseData => {
+  console.log('success', responseData)
+  // $('#message').removeClass()
+  $('#message').text('Signed up successfully!')
+  // $('#message').addClass('success')
+}
+
+const onSignUpFailure = responseData => {
+  console.log('failure', responseData)
+  $('#message').text('Signed up failed!')
+  // $('#message').addClass('failure')
+}
+
+const onSignInSuccess = responseData => {
+  console.log('success', responseData)
+  // $('#message').removeClass()
+  $('#message').text('Signed in successfully!')
+  // $('#message').addClass('success')
+
+  store.user = responseData.user
+}
+
+const onSignInFailure = responseData => {
+  console.log('failure', responseData)
+  $('#message').text('Signed in failed!')
+  // $('#message').addClass('failure')
+}
+
+const onSignOutSuccess = responseData => {
+  console.log('success', responseData)
+  // $('#message').removeClass()
+  $('#message').text('Signed out successfully!')
+  // $('#message').addClass('success')
+}
+
+const onSignOutFailure = responseData => {
+  console.log('failure', responseData)
+  $('#message').text('Signed out failed!')
+  // $('#message').addClass('failure')
+}
+
+module.exports = {
+  onSignUpSuccess,
+  onSignUpFailure,
+  onSignInSuccess,
+  onSignInFailure,
+  onSignOutSuccess,
+  onSignOutFailure
+}
