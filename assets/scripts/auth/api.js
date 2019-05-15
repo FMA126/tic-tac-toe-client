74700f79a29af5af0d04d98b279e0ba0f9820962
FMA126 <mhblanks@gmail.com>
2019-05-15 14:19:11 -0400

    Created user authentication
    Feature for sign up, sign in, sign out.  Curl scripts used for testing.
    Added new folder auth in assets/scripts to hold api, events, and ui
    files.  Html is basic and displays status message

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const store = require('../store')
+const config = require('../config')
+
+const signUp = formData => {
+  console.log('from api signUp')
+
+  return $.ajax({
+    url: config.apiUrl + '/sign-ups',
+    method: 'POST',
+    data: formData
+  })
+}
+
+const signIn = formData => {
+  console.log('from api signIn')
+
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data: formData
+  })
+}
+
+const signOut = () => {
+  console.log('from api signOut')
+
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  signOut
+}
---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const getFormFields = require('../../../lib/get-form-fields.js')
+const api = require('./api')
+const ui = require('./ui')
+
+const onSignUp = event => {
+  event.preventDefault()
+
+  const form = event.target
+  const formData = getFormFields(form)
+
+  console.log('sign up pressed', formData)
+  api.signUp(formData)
+    .then(ui.onSignUpSuccess)
+    .catch(ui.onSignUpFailure)
+}
+
+const onSignIn = event => {
+  event.preventDefault()
+
+  const form = event.target
+  const formData = getFormFields(form)
+
+  console.log('sign in pressed', formData)
+  api.signIn(formData)
+    .then(ui.onSignInSuccess)
+    .catch(ui.onSignUpFailure)
+}
+
+const onSignOut = event => {
+  event.preventDefault()
+
+  console.log('sign out pressed')
+  api.signOut()
+    .then(ui.onSignOutSuccess)
+    .catch(ui.onSignOutFailure)
+}
+
+module.exports = {
+  onSignUp,
+  onSignIn,
+  onSignOut
+}
